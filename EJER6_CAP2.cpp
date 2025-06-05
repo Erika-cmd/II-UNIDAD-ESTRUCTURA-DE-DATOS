1585a218f08be6fd46e1c66196bade46f924f509
Erika-cmd <zenaidavalenciacondori@gmail.com>
2025-06-05 12:21:41 -0500

    Add files via upload

---
+++ EJER6_CAP2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> permutation;
+    for (int i = 1; i <= n; i++) {
+        permutation.push_back(i);
+    }
+    do {
+        for (size_t i = 0; i < permutation.size(); i++) {
+            cout << permutation[i] << " ";
+        }
+        cout << "\n";
+    } while (next_permutation(permutation.begin(), permutation.end()));
+    return 0;
+}
+
